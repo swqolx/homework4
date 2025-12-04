da20bc937d28d54c2982cfcac6531033cea43ba2
ойойой <153764765+swqolx@users.noreply.github.com>
2025-12-04 18:47:03 +0200

    end

---
+++ hw4.js
@@ -0,0 +1,52 @@
+//===1
+// const text1 = ""
+// const text2 = "lala"
+// if (text1 == "" || text2 == ""){
+//     alert("Не всі поля заповнені")
+// }else{
+//     alert("Обидва поля заповнені")
+// }
+
+
+
+//==2
+
+// const num1 = 4
+// const num2 = 12
+// if(num1 + num2 >= 10){
+//     alert("Сума більша за 10")
+// }else{
+//     alert( "Сума менша або дорівнює 10")
+// }
+
+
+//==3
+
+// const text ="kidcjxkmJavaScriptdm ;m v"
+
+// if(text.includes("JavaScript")){
+// alert("Текст містить слово JavaScript")
+// }else{
+//     alert("Текст не містить слово JavaScript")
+// }
+
+//==4
+
+// const num = 34
+// if(num >= 10 && num <= 20){
+//     alert( "Число входить в діапазон від 10 до 20")
+// }else{
+//     alert("Число не входить в діапазон від 10 до 20")
+// }
+
+//==5
+
+// const namee = "Anastasia"
+// const email = "nsjs457@gmail.com"
+// const password = "n65yej9v77"
+// if(namee.length >= 3 && email.includes("@")== true && password.length >= 6){
+//     alert("Перенаправлення на іншу сторінку")
+// }else{
+//     alert("Помилка: неправильне заповнення")
+// }
+
